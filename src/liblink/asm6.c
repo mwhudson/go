c6378be525dee4b9756aa20db336e17bc97ff183
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-01-22 13:57:24 +1300

    liblink, cmd/6l: re-enable -shared on amd64
    Probably not ready to merge, but would be interested in comments.
    
    The creation of liblink and subsequent introduction of more explicit
    TLS handling broke 6l's (unsupported) -shared flag.  This change adds
    -shared flags to 6a and 6g and changes liblink to generate shared-
    library compatible instruction sequences when they are passed, and
    changes 6l to emit the appropriate ELF relocation.
    
    The resulting shared libraries have thousand of relocations against the
    text segment, but at least the external link does not fail any more.
    
    A proper fix probably also requires go tool changes.
    
    Fixes issue #9652.

--- src/liblink/asm6.c
+++ src/liblink/asm6.c
@@ -1900,11 +1900,14 @@ prefixof(Link *ctxt, Addr *a)
 			// a register and then index from that register, do not reach
 			// this code and should not be listed.
 			switch(ctxt->headtype) {
+			case Hlinux:
+				if(!ctxt->flag_shared) {
+					return 0x64; // FS
+				} // else fall through
 			default:
 				sysfatal("unknown TLS base register for %s", headstr(ctxt->headtype));
 			case Hdragonfly:
 			case Hfreebsd:
-			case Hlinux:
 			case Hnetbsd:
 			case Hopenbsd:
 			case Hsolaris:
@@ -1921,6 +1924,13 @@ prefixof(Link *ctxt, Addr *a)
 		return 0x3e;
 	case REG_ES:
 		return 0x26;
+	case REG_TLS:
+		if(ctxt->headtype != Hlinux || !ctxt->flag_shared) {
+			sysfatal("cannot handle index == REG_TLS in this mode");
+		}
+		if(a->offset != 0) {
+			sysfatal("cannot handle handle non-0 offsets to TLS");
+		} // else fall through
 	case REG_FS:
 		return 0x64;
 	case REG_GS:
@@ -2459,7 +2469,7 @@ asmandsz(Link *ctxt, Prog *p, Addr *a, int r, int rex, int m64)
 	}
 
 	if(REG_AX <= base && base <= REG_R15) {
-		if(a->index == REG_TLS) {
+		if(a->index == REG_TLS && !ctxt->flag_shared) {
 			memset(&rel, 0, sizeof rel);
 			rel.type = R_TLS_IE;
 			rel.siz = 4;
@@ -3389,13 +3399,41 @@ mfound:
 			}
 		}
 		break;
-	
+
 	case 7:	/* mov tls, r */
 		// NOTE: The systems listed here are the ones that use the "TLS initial exec" model,
 		// where you load the TLS base register into a register and then index off that
 		// register to access the actual TLS variables. Systems that allow direct TLS access
 		// are handled in prefixof above and should not be listed here.
 		switch(ctxt->headtype) {
+		case Hlinux:
+			if(ctxt->flag_shared) {
+				Reloc *r;
+				// Note that this is not generating the same insn as the other cases.
+				// MOV TLS, R_to becomes
+				// movq g@gottpoff(%rip), R_to
+				// which is encoded as movq 0(%rip), R_to and a R_TLS_IE reloc.
+				// This all assumes the only tls variable we access is g, which we
+				// can't check here, but will when we assemble the second
+				// instruction.
+
+				ctxt->rexflag = Pw | (regrex[p->to.reg] & Rxr);
+
+				*ctxt->andptr++ = 0x8B;
+				*ctxt->andptr++ = 0x05 | (reg[p->to.reg]<<3);
+				r = addrel(ctxt->cursym);
+				r->off = p->pc + ctxt->andptr - ctxt->and;
+				r->type = R_TLS_IE;
+				r->siz = 4;
+				*ctxt->andptr++ = 0x00;
+				*ctxt->andptr++ = 0x00;
+				*ctxt->andptr++ = 0x00;
+				*ctxt->andptr++ = 0x00;
+
+				break;
+			}
+			// else fall through
+
 		default:
 			sysfatal("unknown TLS base location for %s", headstr(ctxt->headtype));
 
