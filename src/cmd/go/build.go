4c48ceeb94eeba93d0b4bf8b0c3252f162d8ddeb
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-02-10 12:46:54 +1300

    cmd/go: support -asmflags to allow passing flags to the assemblers
    Change-Id: Id533e055806ddbe9bb0eafc6f7b9abdba9da8873

--- src/cmd/go/build.go
+++ src/cmd/go/build.go
@@ -74,6 +74,8 @@ and test commands:
 	-x
 		print the commands.
 
+	-asmflags 'arg list'
+		arguments to pass on each 5a, 6a, 8a, or 9a assembler invocation.
 	-compiler name
 		name of compiler to use, as in runtime.Compiler (gccgo or gc).
 	-gccgoflags 'arg list'
@@ -129,6 +131,7 @@ var buildI bool               // -i flag
 var buildO = cmdBuild.Flag.String("o", "", "output file")
 var buildWork bool           // -work flag
 var buildGcflags []string    // -gcflags flag
+var buildAsmflags []string   // -asmflags flag
 var buildLdflags []string    // -ldflags flag
 var buildGccgoflags []string // -gccgoflags flag
 var buildRace bool           // -race flag
@@ -180,6 +183,7 @@ func addBuildFlags(cmd *Command) {
 	cmd.Flag.BoolVar(&buildX, "x", false, "")
 	cmd.Flag.BoolVar(&buildWork, "work", false, "")
 	cmd.Flag.Var((*stringsFlag)(&buildGcflags), "gcflags", "")
+	cmd.Flag.Var((*stringsFlag)(&buildAsmflags), "asmflags", "")
 	cmd.Flag.Var((*stringsFlag)(&buildLdflags), "ldflags", "")
 	cmd.Flag.Var((*stringsFlag)(&buildGccgoflags), "gccgoflags", "")
 	cmd.Flag.Var((*stringsFlag)(&buildContext.BuildTags), "tags", "")
@@ -1679,7 +1683,10 @@ func (gcToolchain) asm(b *builder, p *Package, obj, ofile, sfile string) error {
 	// Add -I pkg/GOOS_GOARCH so #include "textflag.h" works in .s files.
 	inc := filepath.Join(goroot, "pkg", fmt.Sprintf("%s_%s", goos, goarch))
 	sfile = mkAbs(p.Dir, sfile)
-	return b.run(p.Dir, p.ImportPath, nil, stringList(buildToolExec, tool(archChar+"a"), "-trimpath", b.work, "-I", obj, "-I", inc, "-o", ofile, "-D", "GOOS_"+goos, "-D", "GOARCH_"+goarch, sfile))
+	args := []string{"-trimpath", b.work, "-I", obj, "-I", inc, "-o", ofile, "-D", "GOOS_" + goos, "-D", "GOARCH_" + goarch}
+	args = append(args, buildAsmflags...)
+	args = append(args, sfile)
+	return b.run(p.Dir, p.ImportPath, nil, stringList(buildToolExec, tool(archChar+"a")), args)
 }
 
 func (gcToolchain) pkgpath(basedir string, p *Package) string {
