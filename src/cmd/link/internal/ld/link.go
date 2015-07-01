1517e1cf9465f3adbfb2344543dad73c97c14996
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-07-02 11:37:51 +1200

    on arm64, look at the R_AARCH64_RELATIVE relocs to find the gcdata
    Change-Id: I5a1864a27ad917aa65c8e65a133f6cc0a980d05f

--- src/cmd/link/internal/ld/link.go
+++ src/cmd/link/internal/ld/link.go
@@ -114,10 +114,11 @@ type Auto struct {
 }
 
 type Shlib struct {
-	Path string
-	Hash []byte
-	Deps []string
-	File *elf.File
+	Path             string
+	Hash             []byte
+	Deps             []string
+	File             *elf.File
+	gcdata_addresses map[*LSym]uint64
 }
 
 type Link struct {
