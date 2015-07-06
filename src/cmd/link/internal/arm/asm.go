5a2cb812bf5644790f03d3931a281532a4b7a0c6
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-07-06 15:45:34 +1200

    one more
    Change-Id: I4a85afd9945c161735bfd5fb28fc5fb32046f291

--- src/cmd/link/internal/arm/asm.go
+++ src/cmd/link/internal/arm/asm.go
@@ -232,7 +232,7 @@ func elfreloc1(r *ld.Reloc, sectoff int64) int {
 
 	case obj.R_TLS:
 		if r.Siz == 4 {
-			if ld.Buildmode == ld.BuildmodeCShared {
+			if ld.Buildmode == ld.BuildmodeCShared || ld.DynlinkingGo() {
 				ld.Thearch.Lput(ld.R_ARM_TLS_IE32 | uint32(elfsym)<<8)
 			} else {
 				ld.Thearch.Lput(ld.R_ARM_TLS_LE32 | uint32(elfsym)<<8)
