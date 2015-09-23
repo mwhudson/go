aa7e69e28dab119a5983915b98c1b75dfefc7c65
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-09-23 21:02:50 +1200

    cmd/compile: allow -shared/-dynlink on ppc64
    Only effect is register related: do not allocate R2 or R12, put function
    entrypoint in R12 before indirect call.
    
    Change-Id: I9cdd553bab022601c9cb5bb43c9dc0c368c6fb0a

--- src/cmd/compile/internal/ppc64/gsubr.go
+++ src/cmd/compile/internal/ppc64/gsubr.go
@@ -580,6 +580,18 @@ func rawgins(as int, f *gc.Node, t *gc.Node) *obj.Prog {
 	case obj.ACALL:
 		if p.To.Type == obj.TYPE_REG && p.To.Reg != ppc64.REG_CTR {
 			// Allow front end to emit CALL REG, and rewrite into MOV REG, CTR; CALL CTR.
+			if gc.Ctxt.Flag_dynlink {
+				// Make sure function pointer is in R12 as well when
+				// dynamically linking Go.
+				// TODO(mwhudson): it would obviously be better to
+				// change the register allocation to put the value in
+				// R12 already, but I don't know how to do that.
+				p := gc.Prog(as)
+				p.As = ppc64.AMOVD
+				p.From = p.To
+				p.To.Type = obj.TYPE_REG
+				p.To.Reg = ppc64.REG_R12
+			}
 			pp := gc.Prog(as)
 			pp.From = p.From
 			pp.To.Type = obj.TYPE_REG
