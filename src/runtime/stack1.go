92d141c2638dde44917dd839fc2d90b8f46147f6
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-10-02 13:50:32 +1300

    quieter
    Change-Id: I42b4204a4d5f51dec32dc4c4ecf828bd7f4d3480

--- src/runtime/stack1.go
+++ src/runtime/stack1.go
@@ -12,10 +12,10 @@ const (
 	//            == 2: logging of per-frame operations
 	//            == 3: logging of per-word updates
 	//            == 4: logging of per-word reads
-	stackDebug       = 4 * shared
-	stackFromSystem  = 0          // allocate stacks from system memory instead of the heap
-	stackFaultOnFree = 0          // old stacks are mapped noaccess to detect use after free
-	stackPoisonCopy  = 1 * shared // fill stack that should not be accessed with garbage, to detect bad dereferences during copy
+	stackDebug       = 0
+	stackFromSystem  = 0 // allocate stacks from system memory instead of the heap
+	stackFaultOnFree = 0 // old stacks are mapped noaccess to detect use after free
+	stackPoisonCopy  = 0 // fill stack that should not be accessed with garbage, to detect bad dereferences during copy
 
 	stackCache = 1
 )
