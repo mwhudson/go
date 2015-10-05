d438558dc33a2eb95946b11aa8d4f33108f56a52
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-10-05 22:07:09 +1300

    wtf
    Change-Id: I12b2e06ef640de94487b0e128b05e5db08992ed7

--- src/reflect/type.go
+++ src/reflect/type.go
@@ -1862,7 +1862,7 @@ func ArrayOf(count int, elem Type) Type {
 	ckey := cacheKey{Array, typ, nil, uintptr(count)}
 	if array := cacheGet(ckey); array != nil {
 		println("found by cacheGet")
-		a := array.(*arraytTpe)
+		a := array.(*arrayType)
 		ckey2 := cacheKey{array.Kind(), a.elem, nil, a.size / a.elem.size}
 		if ckey != ckey2 {
 			println("wtf")
