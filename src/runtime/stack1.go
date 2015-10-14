c1bc81a487af02e8e501301d3377c6332cae1088
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-10-15 10:28:34 +1300

    stackDebug
    Change-Id: I8d786dd124cf47df8a5f158303f72b69a740368b

--- src/runtime/stack1.go
+++ src/runtime/stack1.go
@@ -12,7 +12,7 @@ const (
 	//            == 2: logging of per-frame operations
 	//            == 3: logging of per-word updates
 	//            == 4: logging of per-word reads
-	stackDebug       = 0
+	stackDebug       = 2
 	stackFromSystem  = 0 // allocate stacks from system memory instead of the heap
 	stackFaultOnFree = 0 // old stacks are mapped noaccess to detect use after free
 	stackPoisonCopy  = 0 // fill stack that should not be accessed with garbage, to detect bad dereferences during copy
