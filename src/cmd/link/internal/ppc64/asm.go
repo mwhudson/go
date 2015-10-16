CongLeSolutionX/go_community

cmd/internal/obj, cmd/link, runtime: handle TLS more like a platform linker on ppc64
We need to do this for external linking.

Change-Id: I2556c249ab2d802cae62c44b2b4c5b44787d7059

src/cmd/link/internal/ppc64/asm.go
@@ -401,6 +401,15 @@ func archreloc(r *ld.Reloc, s *ld.LSym, val *int64) int {
 		*val = ld.Symaddr(r.Sym) + r.Add - symtoc(s)
 
 		return 0
+
+	case obj.R_PPC64_TPREL16:
+		v := r.Sym.Value - 0x7000
+		if v < -32678 || v >= 32678 {
+			ld.Diag("TLS offset out of range %d", v)
+		}
+		*val |= v & 0xffff
+		return 0
+
 	}
 
 	return -1

mwhudson/go

src/cmd/link/internal/ppc64/asm.go
@@ -401,6 +401,14 @@ func archreloc(r *ld.Reloc, s *ld.LSym, val *int64) int {
 		*val = ld.Symaddr(r.Sym) + r.Add - symtoc(s)
 
 		return 0
+
+	case obj.R_POWER_TLS_LE:
+		v := r.Sym.Value - 0x7000
+		if v < -32678 || v >= 32678 {
+			ld.Diag("TLS offset out of range %d", v)
+		}
+		*val |= v & 0xffff
+		return 0
 	}
 
 	return -1