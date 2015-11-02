e6ac75fa674f1deeb523cc61c9324051775601f6
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-11-02 13:39:15 +1300

    doh
    Change-Id: I7a1b3f973d2f59c32e24a8d82be1f19c2919916a

--- src/cmd/link/internal/ld/data.go
+++ src/cmd/link/internal/ld/data.go
@@ -355,7 +355,7 @@ func relocsym(s *LSym) {
 
 		// We need to be able to reference dynimport symbols when linking against
 		// shared libraries, and Solaris needs it always
-		if HEADTYPE != obj.Hsolaris && r.Sym != nil && r.Sym.Type == obj.SDYNIMPORT && !DynlinkingGo() {
+		if HEADTYPE != obj.Hsolaris && r.Sym != nil && r.Sym.Type == obj.SDYNIMPORT && !(DynlinkingGo() || Buildmode == BuildmodePIE) {
 			Diag("unhandled relocation for %s (type %d rtype %d)", r.Sym.Name, r.Sym.Type, r.Type)
 		}
 		if r.Sym != nil && r.Sym.Type != obj.STLSBSS && !r.Sym.Reachable {
